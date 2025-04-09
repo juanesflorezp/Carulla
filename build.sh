77c2abb650de35ca1b65cae7abcc16355d1141d0
juanesflorezp <juanesflorezp@gmail.com>
2025-04-09 11:46:00 -0500

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Instala Google Chrome
+apt-get update
+apt-get install -y wget unzip gnupg
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+apt-get update
+apt-get install -y google-chrome-stable
+
+# Da permisos al binario
+chmod +x /usr/bin/google-chrome
