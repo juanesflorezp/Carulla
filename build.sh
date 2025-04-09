5ead8e3ed477adae0937083f747ce2c286ee37ba
juanesflorezp <juanesflorezp@gmail.com>
2025-04-09 11:59:02 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y curl unzip gnupg
+set -e
 
-# Agregar repositorio oficial de Google Chrome
-curl -sSL https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+apt-get update
+apt-get install -y wget gnupg2 curl unzip
+
+# Agregar la clave pública y repositorio oficial de Chrome
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
 
-# Instalar Google Chrome estable
+# Instalar Chrome estable
 apt-get update
 apt-get install -y google-chrome-stable
 
-# Hacer alias para asegurarse de que esté disponible en /usr/bin/google-chrome
-ln -s /usr/bin/google-chrome-stable /usr/bin/google-chrome
+# Verificar instalación
+which google-chrome-stable
+google-chrome-stable --version
 
+# Hacer alias si no existe el binario esperado
+ln -sf /usr/bin/google-chrome-stable /usr/bin/google-chrome
